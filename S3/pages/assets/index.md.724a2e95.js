722733e115a5f65b8325ee15d1ebac54a94219c0
rohanharikr <rohanharikumar80@gmail.com>
2022-08-03 15:18:14 +0400

    init vitepess at /pages/

---
+++ S3/pages/assets/index.md.724a2e95.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.efc18d5b.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),r={name:"index.md"};function n(o,s,c,d,i,p){return a(),t("div")}var f=e(r,[["render",n]]);export{m as __pageData,f as default};
---
+++ pages-src/docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+    title: 'Hellō Docs',
+    lang: 'en-US',
+    outDir: '../../S3/pages',
+    base: '/pages/',
+    themeConfig: {
+        siteTitle: 'Hellō Docs',
+        sidebar: [
+            {
+                items: [
+                    { text: 'Terms of Service', link: '/terms-of-service' },
+                    { text: 'Privacy Policy', link: '/privacy-policy' }
+                ]
+            }
+        ],
+        editLink: {
+            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+            text: 'Propose changes to this page'
+        }
+    }
+}
