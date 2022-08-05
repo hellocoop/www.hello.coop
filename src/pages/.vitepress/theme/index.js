c362147c2142d4cb939471c193f2545acdb7b9b6
rohanharikr <rohanharikumar80@gmail.com>
2022-08-05 15:44:20 +0400

    fix

--- src/pages/.vitepress/theme/index.js
+++ src/pages/.vitepress/theme/index.js
@@ -1,26 +1,29 @@
 import defaultTheme from 'vitepress/theme'
 import MyLayout from './MyLayout.vue'
 import './vars.css'
+import { onMounted } from 'vue'
 
-// const root = document.getElementsByTagName( 'html' )[0];
+onMounted(() => {
+  const root = document.getElementsByTagName( 'html' )[0];
 
-// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//   root.setAttribute( 'class', 'dark' );
-// }
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    root.setAttribute( 'class', 'dark' );
+  }
 
-// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-//   const newColorScheme = event.matches ? "dark" : "light";
-//   switch(newColorScheme){
-//     case 'light': {
-//       root.setAttribute( 'class', 'light' );
-//       break;
-//     }
-//     case 'dark': {
-//       root.setAttribute( 'class', 'dark' );
-//       break;
-//     }
-//   }
-// });
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    switch(newColorScheme){
+      case 'light': {
+        root.setAttribute( 'class', 'light' );
+        break;
+      }
+      case 'dark': {
+        root.setAttribute( 'class', 'dark' );
+        break;
+      }
+    }
+  });
+})
 
 export default {
   ...defaultTheme,
