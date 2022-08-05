6b84f87e8fe8de8c7ab70fbf6db2d5ff1d8b06a9
rohanharikr <rohanharikumar80@gmail.com>
2022-08-05 14:54:09 +0400

    auto theme switch

--- src/pages/.vitepress/theme/index.js
+++ src/pages/.vitepress/theme/index.js
@@ -1,6 +1,26 @@
 import defaultTheme from 'vitepress/theme'
 import './vars.css'
 
+const root = document.getElementsByTagName( 'html' )[0];
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  root.setAttribute( 'class', 'dark' );
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  const newColorScheme = event.matches ? "dark" : "light";
+  switch(newColorScheme){
+    case 'light': {
+      root.setAttribute( 'class', 'light' );
+      break;
+    }
+    case 'dark': {
+      root.setAttribute( 'class', 'dark' );
+      break;
+    }
+  }
+});
+
 export default {
   ...defaultTheme
 }
