e937f2ffe6f3f39dba3f730ae93dafff5b969401
rohanharikr <rohanharikumar80@gmail.com>
2023-09-06 17:14:26 +0530

    remove sending slack notification from local script

--- scripts/review.mjs
+++ scripts/review.mjs
@@ -1,8 +1,4 @@
 #!/usr/bin/env zx
-import { SLACK_ACCESS_TOKEN } from "../vault.mjs"
-
-const SLACK_CHANNEL = "test-dev-process"
-
 try {
     // $.verbose = false //do not log output of script run using zx
 
@@ -29,17 +25,10 @@ try {
         isExistingPR = true
     }
     
-    //Get PR details for Slack notification
-    const prUrlPromise = await $`gh pr list --base main --head ${prBranchName} --json url`
-    const prUrl = JSON.parse(prUrlPromise.stdout)[0].url
-    const repoDetailsPromise = await $`gh repo view --json name --json url`
-    const { name: repoName } = JSON.parse(repoDetailsPromise.stdout)
-    const prAuthorName = (await $`git config user.name`).stdout.trim()
-    
-    await sendSlackMessage({repoName, prBranchName, isExistingPR, prAuthorName, prUrl})
-    
     //Play success sound to notify (Only works on MacOS)
     $`afplay /System/Library/Sounds/Glass.aiff`
+
+    //GitHub-Slack app sends a PR created Slack message to #content-updates channel
 } catch(p) {
     if(p.stderr) { //Error thrown by zx
         console.log(`Exit code: ${p.exitCode}`)
@@ -50,35 +39,4 @@ try {
     //Play error sound to notify (Only works on MacOS)
     $`afplay /System/Library/Sounds/Funk.aiff`
     process.exit(1)
-}
-
-async function sendSlackMessage ({
-    repoName,
-    prBranchName,
-    isExistingPR,
-    prAuthorName,
-    prUrl
-} = {}) {
-    const msg = JSON.stringify({        
-        channel: SLACK_CHANNEL,
-        blocks: [
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: 
-`<${prUrl}|${repoName}/${prBranchName}> pull request was ${isExistingPR ? "updated" : "created"} by @${prAuthorName}
-*Waiting for Review and Approval*`
-                }
-            }
-        ]
-    })
-    await fetch("https://slack.com/api/chat.postMessage", {
-        method: "POST",
-        headers: {
-            "Authorization": "Bearer " + SLACK_ACCESS_TOKEN,
-            "Content-Type": "application/json"
-        },
-        body: msg
-    })
 }
