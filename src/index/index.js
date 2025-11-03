d10f27062aab11e5158f710dce61182f9437ba13
rohanharikr <rohanharikumar80@gmail.com>
2025-11-03 17:00:54 +0000

    Remove AOS library and related animations from index.js and HTML for cleaner implementation
    - Deleted AOS import and initialization from index.js to streamline animation handling.
    - Removed data-aos attributes from images in index.html to eliminate unused animation triggers.
    - Updated package.json and package-lock.json to reflect the removal of AOS dependency.

--- src/index/index.js
+++ src/index/index.js
@@ -1,21 +1,8 @@
-import AOS from 'aos';
-import { animate, createTimeline, svg } from 'animejs';
-import 'aos/dist/aos.css';
+import { createTimeline, svg } from 'animejs';
 
 window.onload = async () => {
     processFeed();
     handleDropdown();
-
-    AOS.init({
-        offset: 500, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 1000, // values from 0 to 3000, with step 50ms
-        easing: "ease", // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-    });
-
     orgsHaveControlAnimation();
 }
 
