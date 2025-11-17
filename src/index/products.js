ddecc26bc9465a8ad49860a1656648141d167f15
rohanharikr <rohanharikumar80@gmail.com>
2025-11-17 10:14:49 +0000

    Add plausible analytics initialization on products page
    - Introduced a window load event listener to initialize plausible analytics for enhanced tracking capabilities on the products page.
    - Ensured compatibility with existing analytics setup by checking for the plausible function before initialization.

--- src/index/products.js
+++ src/index/products.js
@@ -1,1 +1,5 @@
 // products page specific scripts
+window.addEventListener('load', () => {
+    window.plausible = window.plausible || function () { (plausible.q = plausible.q || []).push(arguments) }, plausible.init = plausible.init || function (i) { plausible.o = i || {} };
+    plausible.init()
+});
