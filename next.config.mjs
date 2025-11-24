dd296d11b0c488f91c8efcf3d8020a686c58e795
rohanharikr <rohanharikumar80@gmail.com>
2025-11-24 12:25:26 +0000

    Remove commented-out contentDirBasePath from Nextra configuration in next.config.mjs for cleaner code.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import nextra from 'nextra'
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  // contentDirBasePath: '/pages',
   // ... Add Nextra-specific options here
 })
 
